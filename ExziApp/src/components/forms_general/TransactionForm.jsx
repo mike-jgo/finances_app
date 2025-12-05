92e1f54334fab0fa5ef2d6b0260a3a1fccdbbcb3
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-12-06 03:18:06 +0800

    Update TransactionForm.jsx

--- ExziApp/src/components/forms_general/TransactionForm.jsx
+++ ExziApp/src/components/forms_general/TransactionForm.jsx
@@ -1,15 +1,15 @@
 import { Field, Input, Textarea, Switch } from '@headlessui/react'
 import TranscDatePicker from '../form_components/TranscDatePicker'
-import {DatePicker, IncomeSourceField, ExpenseCategoryField, TransferSourceField, TransferDestField} from './TransactionFormComponents'
+import { DatePicker, IncomeSourceField, ExpenseCategoryField, TransferSourceField, TransferDestField } from './TransactionFormComponents'
 
-const TransactionForm = ({transaction_type, inc_cat_label, exp_src_label, exp_dst_label, transfer_src_label, transfer_dst_label}) => {
+const TransactionForm = ({ transaction_type, inc_cat_label, exp_src_label, exp_dst_label, transfer_src_label, transfer_dst_label, categories, expenseCategories }) => {
 
     const renderSection = () => {
         switch (transaction_type) {
             case 'income':
                 return (
                     <>
-                        <IncomeSourceField transaction_type={transaction_type} inc_cat_label={inc_cat_label}/>
+                        <IncomeSourceField transaction_type={transaction_type} inc_cat_label={inc_cat_label} categories={categories} />
                         <DatePicker transaction_type={transaction_type} />
                     </>
                 )
@@ -17,8 +17,8 @@ const TransactionForm = ({transaction_type, inc_cat_label, exp_src_label, exp_ds
             case 'expense':
                 return (
                     <>
-                        <IncomeSourceField transaction_type={transaction_type} inc_cat_label={inc_cat_label}/>
-                        <ExpenseCategoryField exp_dst_label={exp_dst_label} />
+                        <IncomeSourceField transaction_type={transaction_type} inc_cat_label={inc_cat_label} categories={categories} name="inccat_id" />
+                        <ExpenseCategoryField exp_dst_label={exp_dst_label} categories={expenseCategories} />
                         <DatePicker transaction_type={transaction_type} />
                     </>
                 )
@@ -34,16 +34,17 @@ const TransactionForm = ({transaction_type, inc_cat_label, exp_src_label, exp_ds
 
     return (
         <div className="h-full flex flex-col p-4 gap-4">
+            <input type="hidden" name="transaction_type" value={transaction_type} />
             <div className="flex justify-between w-full gap-4 shrink-0">
                 {renderSection()}
             </div>
             <Field as="div" className="flex flex-col shrink-0">
                 <label>Enter amount</label>
-                <Input className="p-4 border border-[#646464] rounded-sm hover:border hover:border-white hover:transition duration-300 ease-in-out grow"/>
+                <Input name="amount" type="number" step="0.01" className="p-4 border border-[#646464] rounded-sm hover:border hover:border-white hover:transition duration-300 ease-in-out grow" />
             </Field>
             <Field as="div" className="flex flex-col shrink-0 grow">
                 <label>Enter note</label>
-                <Textarea className="p-4 border border-[#646464] rounded-sm hover:border hover:border-white hover:transition duration-300 ease-in-out grow"/>
+                <Textarea name="note" className="p-4 border border-[#646464] rounded-sm hover:border hover:border-white hover:transition duration-300 ease-in-out grow" />
             </Field>
         </div>
     )
