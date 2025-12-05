0cfa785b61126fdc1e0bc7c1e91aa2526f65b890
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-12-06 03:18:10 +0800

    Update CategorySelector.jsx

--- ExziApp/src/components/form_components/CategorySelector.jsx
+++ ExziApp/src/components/form_components/CategorySelector.jsx
@@ -1,17 +1,28 @@
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-const CategorySelector = ({ categoryList }) => {
-    const [currCategory, setCurrCategory] = useState(categoryList[0])
+const CategorySelector = ({ categoryList, name }) => {
+    const [currCategory, setCurrCategory] = useState(categoryList?.[0] || { name: 'Select Category', id: '' })
+
+    // Update selected category when list populates if currently not set or invalid
+    useEffect(() => {
+        const currentId = currCategory.id || currCategory.inccatid;
+        if (categoryList?.length > 0 && (!currentId || !categoryList.find(c => (c.id || c.inccatid) === currentId))) {
+            setCurrCategory(categoryList[0])
+        }
+    }, [categoryList, currCategory])
 
     return (
-        <Listbox as="div" className="w-full h-full" value={currCategory} onChange={setCurrCategory}>
-            <ListboxButton className="w-full h-full border border-[#646464] rounded-sm data-focus:border-white hover:border hover:border-white hover:transition duration-300 ease-in-out">{currCategory.name}</ListboxButton>
+        <Listbox as="div" className="w-full h-full" value={currCategory} onChange={setCurrCategory} name={name}>
+            <input type="hidden" name={name} value={currCategory.id || currCategory.inccatid} />
+            <ListboxButton className="w-full h-full border border-[#646464] rounded-sm data-focus:border-white hover:border hover:border-white hover:transition duration-300 ease-in-out">
+                {currCategory.name || currCategory.title || currCategory.categoryName || 'Select Category'}
+            </ListboxButton>
             <ListboxOptions anchor="bottom" className="z-[1001] w-(--button-width) border border-[#646464] rounded-sm">
-                {categoryList.map((person) => (
-                <ListboxOption key={person.id} value={person} className="bg-black data-focus:bg-gray-900">
-                    {person.name}
-                </ListboxOption>
+                {(categoryList || []).map((category) => (
+                    <ListboxOption key={category.id} value={category} className="bg-black data-focus:bg-gray-900">
+                        {category.name || category.title || category.categoryName}
+                    </ListboxOption>
                 ))}
             </ListboxOptions>
         </Listbox>
