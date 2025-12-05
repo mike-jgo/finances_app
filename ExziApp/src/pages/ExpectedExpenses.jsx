e0bc9e0beb8e6d17c02984101a79b652152cd120
Michael Joseph Go <135100340+mike-jgo@users.noreply.github.com>
2025-12-05 12:50:13 +0800

    Update ExpectedExpenses.jsx

--- ExziApp/src/pages/ExpectedExpenses.jsx
+++ ExziApp/src/pages/ExpectedExpenses.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useCallback, useEffect, useMemo, useState } from "react"
 import { useOutletContext } from 'react-router'
 import Button from "../components/Button.jsx"
-import { ModalContext } from "../contexts/ModalContext.js"
+import { ModalContext } from "../contexts/ModalContext.jsx"
 import TransactionForm from "../components/forms_general/TransactionForm.jsx"
 import ExpectedExpenseCard from "../components/cards/ExpectedExpenseCard.jsx"
 
@@ -79,48 +79,48 @@ const expected_expenses = [
 ]
 
 const ExpectedExpenses = () => {
-    const {setHeaderButton, setModalType, setModalHeader} = useOutletContext()
+  const { setHeaderButton, setModalType, setModalHeader } = useOutletContext()
 
-    const openExpectedExpenseModal = useCallback(() => {
-        setModalType(<TransactionForm transaction_type="expected_expense" inc_cat_label="Select income source" exp_dst_label="Select category"/>)
-        setModalHeader("Add expected expense")
-    }, [setModalHeader, setModalType])
+  const openExpectedExpenseModal = useCallback(() => {
+    setModalType(<TransactionForm transaction_type="expected_expense" inc_cat_label="Select income source" exp_dst_label="Select category" />)
+    setModalHeader("Add expected expense")
+  }, [setModalHeader, setModalType])
 
-    const modalContext = useMemo(() => ({
-      setModalType, 
-      setModalHeader
-    }), [setModalHeader, setModalType])
+  const modalContext = useMemo(() => ({
+    setModalType,
+    setModalHeader
+  }), [setModalHeader, setModalType])
 
-    useEffect(() => {
-        const addExpectedExpenseButton = <Button text="📅 Add expected expense" onClick={openExpectedExpenseModal}></Button>
-        setHeaderButton(addExpectedExpenseButton)
+  useEffect(() => {
+    const addExpectedExpenseButton = <Button text="📅 Add expected expense" onClick={openExpectedExpenseModal}></Button>
+    setHeaderButton(addExpectedExpenseButton)
 
-        return () => {
-            setHeaderButton(null)
+    return () => {
+      setHeaderButton(null)
+    }
+  }, [setHeaderButton, openExpectedExpenseModal])
+
+  return (
+    <main className="h-full w-full overflow-y-scroll">
+      <div className="h-[10%] w-full p-8 sticky top-0 bg-[#0b1215] z-[20]">
+        <div className="grid gap-4 grid-cols-6 grid-rows-1 w-[80%]">
+          <h3 className="col-start-1 col-end-2">Name</h3>
+          <h3 className="col-start-2 col-end-3">Amount</h3>
+          <h3 className="col-start-3 col-end-4">Deadline</h3>
+          <h3 className="col-start-4 col-end-5">Spent from</h3>
+          <h3 className="col-start-5 col-end-6">Recorded to</h3>
+          <h3 className="col-start-6 col-end-7">Status</h3>
+        </div>
+      </div>
+      <div className="flex flex-col w-full h-[90%] gap-4 p-4">
+        {
+          expected_expenses.map((expected_expense) => (
+            <ExpectedExpenseCard key={expected_expense.id} expected_expense={expected_expense} />
+          ))
         }
-    }, [setHeaderButton, openExpectedExpenseModal])
-
-    return (
-        <main className="h-full w-full overflow-y-scroll">
-            <div className="h-[10%] w-full p-8 sticky top-0 bg-[#0b1215] z-[20]">
-                <div className="grid gap-4 grid-cols-6 grid-rows-1 w-[80%]">
-                    <h3 className="col-start-1 col-end-2">Name</h3>
-                    <h3 className="col-start-2 col-end-3">Amount</h3>
-                    <h3 className="col-start-3 col-end-4">Deadline</h3>
-                    <h3 className="col-start-4 col-end-5">Spent from</h3>
-                    <h3 className="col-start-5 col-end-6">Recorded to</h3>
-                    <h3 className="col-start-6 col-end-7">Status</h3>
-                </div>
-            </div>
-            <div className="flex flex-col w-full h-[90%] gap-4 p-4">
-                {
-                    expected_expenses.map((expected_expense) => (
-                        <ExpectedExpenseCard key={expected_expense.id} expected_expense={expected_expense}/>
-                    ))
-                }
-            </div>
-        </main>
-    )
+      </div>
+    </main>
+  )
 }
 
 export default ExpectedExpenses
